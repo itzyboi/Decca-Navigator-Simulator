28b5ce673b7b4cf4fffdd61a1da014dad89a08bd
itzyboi <luke.itzinger@gmail.com>
2018-01-30 13:25:52 +0000

    Haversine distance calculator complete. Radian conversion function complete

--- DECCA/DECCA.ino
+++ DECCA/DECCA.ino
@@ -1,13 +1,15 @@
+#include <math.h> 
+
 /* Frequency Data */
 #define baseFrequency 14   // Base Frequency in kHz
 #define redMultiplier 4
 #define greenMultiplier 5
 #define purpleMultiplier 7
 
-/* Location Data - decimal lat and long - 1 = 1.0274m */
+/* Location Data - decimal lat and long - 1 = 0.71555 */
 #define stepSize 15
 
-#define masterLat 0
+#define masterLat 
 #define masterLong 0
 #define redLat 0
 #define redLong 0
@@ -15,6 +17,9 @@
 #define greenLong 0
 #define purpleLat 0
 #define purpleLong 0
+
+/* Constants */
+#define earthRadiusM 6371000
 
 // Spinnaker tower: 50.79556, -1.10848
 #define spinnakerLat 5079556
@@ -42,18 +47,34 @@ int stepDistance(int start, int target, int steps) {
   return distance/steps;
 }
 
+float toRadians(float x) {
+  /* Provides a good estimate of degree to radian conversion */
+  return (x*71)/4068;
+}
 
+int haversine(float startLat, float startLong, float targetLat, float targetLong) {
+  /* Calculates the distance over the earths surface to get from start to target */
+  float startLatRad = toRadians(startLat /= 100000);
+  float startLongRad = toRadians(startLong /= 100000);
+  float targetLatRad = toRadians(targetLat /= 100000);
+  float targetLongRad = toRadians(targetLong /= 100000);
+
+  
+  
+  float deltaLatRad = targetLatRad - startLatRad;
+  float deltaLongRad = targetLongRad - startLongRad;
+
+  float a = sq(sin(deltaLatRad/2)) + (cos(startLatRad)*cos(targetLatRad)*sq(sin(deltaLongRad/2)));
+  float c = 2 * atan2(sqrt(a),sqrt(1-a));
+  return earthRadiusM * c;
+}
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  int steps = stepNumber(haslarMarinaLat, spinnakerLat);
-  Serial.print("Steps: ");
-  Serial.println(steps);
-  Serial.print("Step Distance: ");
-  Serial.println(stepDistance(haslarMarinaLat, spinnakerLat, steps));
+  Serial.println(haversine(spinnakerLat, spinnakerLong, haslarMarinaLat, haslarMarinaLong));
 }
 
 void loop() {
